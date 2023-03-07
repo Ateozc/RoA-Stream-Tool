e8a63e7e873d363f681f8c9d81f9d00a310ad171
Ateozc <tmstofferahn@gmail.com>
2023-03-06 22:11:47 -0600

    Added full support for the major items. Next update adds toggles

--- Stream Tool/Resources/Scripts/GUI/ScreenRocker.mjs
+++ Stream Tool/Resources/Scripts/GUI/ScreenRocker.mjs
@@ -1,39 +1,47 @@
 import { stPath } from './Globals.mjs';
 import { settings } from './Settings.mjs';
 import { getJson, getPresetList } from './File System.mjs';
-import { players } from './Player/Players.mjs';
+import { players, playersReady } from './Player/Players.mjs';
 import { bestOf } from "./BestOf.mjs";
 import { scores } from './Score/Scores.mjs';
-import { currentColors } from './Colors.mjs';
+import { currentColors, updateColor } from './Colors.mjs';
+import { readyToUpdate, writeScoreboard } from './Write Scoreboard.mjs';
+import { customChange, setCurrentPlayer } from './Custom Skin.mjs';
 
 const colorList = await getJson(stPath.text + "/Color Slots");
 
 class ScreenRocker {
 
-    playerPresets;
-    data = "";
-    enabled = false;
+    #playerPresets = "";
+    #data = "";
+    #enabled = false;
+    #diffFound = false;
+
 
     constructor() {
         
     }
 
-    toggle() {
-        if (this.canToggleOn()) {
-            this.enabled = !this.enabled;
-        } else {
-            this.enabled = false;
-        }
-        return this.enabled;
+    enabled() {
+        return this.#enabled;
     }
 
-    checkValues() {
-        return this.data;
+    hasData() {
+        return (this.#data != "" && this.#playerPresets != "")
+    }
+
+    toggle() {
+        if (this.canToggleOn()) {
+            this.#enabled = !this.#enabled;
+        } else {
+            this.#enabled = false;
+        }
+        return this.#enabled;
     }
 
     async getData() {
-        this.playerPresets = await getPresetList("Player Info");
-        this.data = await getJson(stPath.text + "/RoAState");
+        this.#playerPresets = await getPresetList("Player Info");
+        this.#data = await getJson(stPath.text + "/RoAState");
         
     }
 
@@ -49,89 +57,194 @@ class ScreenRocker {
         return true;
     }
 
-    setData() {
-        
+    async #setCharAndSkinData(playerIndex, char) {
+        if(true) { // update characters
+            let playerPresetSkin = {};
+            playerPresetSkin = this.#getSkinPreset(players[playerIndex].nameInp.value, char);
 
-        /*
-        Fix the set data to only work based upon the active slots (so if p1 fights p4, its still 1v1. Apply correctly)
-        Ensure score works
-        ensure best of swaps correctly
-        */
+            if (players[playerIndex].char != char || players[playerIndex].skin.name != playerPresetSkin.name) {
+                this.#diffFound = true;
+                await players[playerIndex].charChange(char, true);
+                if (playerPresetSkin.customImg) {
+                    setCurrentPlayer(players[playerIndex]);
+                    await customChange(playerPresetSkin.hex, playerPresetSkin.name);
+                } else {
+                    await players[playerIndex].skinChange(players[playerIndex].findSkin(playerPresetSkin.name));
+                }
+            } 
+        }
+        return true;
+    }
 
+    #getSkinPreset(name, char) {
+        let skinObj = {
+            name: 'Default',
+            hex: "",
+            customImg: false
+        }
+        for (let i = 0; i < this.#playerPresets.length; i++) {
+            let player = this.#playerPresets[i];
+            if (player.name == name) {
+                for (let j = 0; j < player.characters.length; j++) {
+                    if (char == player.characters[j].character) {
+
+                        skinObj = {
+                            name: player.characters[j].skin,
+                            hex: (player.characters[j].hex) ? player.characters[j].hex : "",
+                            customImg: (player.characters[j].customImg) ? true : false
+                        }
+                        return skinObj;
+                    }
+                }
+            }
+        }
+        return skinObj;
+    }
+
+    
+    async #setColorData(playerIndex, playerCount, slot, state) {
+        if (true) { //Update team colors
+            let newColor = {};
+            let colorIndex = -1;
+            if (playerIndex == 0) {
+                if (playerCount > 2) { //force red on teams
+                    newColor = this.getColorBasedOnSlot(1, 'HMN');
+                    colorIndex = 0;
+                } else {
+                    newColor = this.getColorBasedOnSlot(slot, state);
+                    colorIndex = 0;
+                }
+            } else if (playerIndex == 1 && playerCount < 3) {
+                newColor = this.getColorBasedOnSlot(slot, state);
+                colorIndex = 1;
+            } else if (playerIndex == 2 && playerCount > 2) {
+                newColor = this.getColorBasedOnSlot(2, 'HMN');
+                colorIndex = 1;
+            }
+
+            if (!this.#isSame(currentColors[colorIndex], newColor)) {
+                await updateColor(colorIndex, newColor);
+            }
+        }
+    }
+
+    getColorBasedOnSlot(slot, state) {
+        if (state == 'CPU' || state == 'OFF') {
+            return this.findColorPreset('CPU');
+        }
+        if (slot == 1) {
+            return this.findColorPreset('Red');
+        }
+    
+        if (slot == 2) {
+            return this.findColorPreset('Blue');
+        }
+    
+        if (slot == 3) {
+            return this.findColorPreset('Pink');
+        }
+    
+        if (slot == 4) {
+            return this.findColorPreset('Green');
+        }
+
+    }
+
+    findColorPreset(name) {
+        for (var i = 0; i < colorList.length; i++) {
+            if (name == colorList[i].name) {
+                return colorList[i];
+            }
+        }
+    }
+
+    async #setScoreData(playerIndex, playerCount, gameCount) {
+        if (true) { //Update Score
+            let scoreIndex = -1;
+            if (playerIndex == 0) {
+                scoreIndex = 0;
+            } else if (playerIndex == 1 && playerCount < 3) {
+                scoreIndex = 1;
+            } else if (playerIndex == 2 && playerCount == 4) {
+                scoreIndex = 1
+            }
+            if (scoreIndex != -1) {
+                if (!this.#isSame(scores[scoreIndex].getScore(), gameCount)) {
+                    scores[scoreIndex].setScore(gameCount); 
+                } 
+            }
+        }
+    }
+
+    async #setBoData(newBo) {
+        if (true) {
+            if (newBo == 3 || newBo == 5) {
+                if (!this.#isSame(bestOf.getBo(), newBo)){
+                    bestOf.setBo(newBo);
+                }
+            }
+        }
+    }
+
+    #writeData() {
+        if (this.#diffFound && playersReady()) {
+            writeScoreboard();
+            document.getElementById('botBar').style.backgroundColor = "var(--bg3)";   
+        }
+    }
+
+    #isSame(item1, item2) {
+        if(typeof item1 == Object) {
+            for (let i in item1) {
+                if(!this.#isSame(item1[i], item2[i])) {
+                    this.#diffFound = true;
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            if (item1 == item2) {
+                return true;
+            }
+            this.#diffFound = true;
+            return false;
+        }
+    }
+
+
+    async setData() {
+        this.#diffFound = false;
         let playerCount = 0;
-        for (let i = 0; i < this.data.Characters.length; i++) {
-            if (this.data.Characters[i].SlotState != 'OFF') {
+        for (let i = 0; i < this.#data.Characters.length; i++) {
+            if (this.#data.Characters[i].SlotState != 'OFF') {
                 playerCount++;
             }
         }
-
-
+        
         //Player Characters
         let playerIndex = 0;
-        for (let i = 0; i < this.data.Characters.length; i++) {
-            let slot = this.data.Characters[i].SlotNumber;
-            let char = capitalizeWords(this.data.Characters[i].Character);
-            let state = this.data.Characters[i].SlotState;
-            let gameCount = this.data.Characters[i].GameCount;
+        for (let i = 0; i < this.#data.Characters.length; i++) {
+            let slot = this.#data.Characters[i].SlotNumber;
+            let char = capitalizeWords(this.#data.Characters[i].Character);
+            let state = this.#data.Characters[i].SlotState;
+            let gameCount = this.#data.Characters[i].GameCount;
 
             if (playerCount > 2 || state != 'OFF') {
-                if(true) { // update characters
-//TODO: Need to find a way to populate with the skin of the player (if possible). Current issue is that the async of charChange means we cannot set a skin at this point in time.
-
-                    players[playerIndex].charChange(char)
-                    // players[playerIndex].charChange(char).then((value) =>
-                    // {
-
-                    //     console.log(players[playerIndex].findSkin('Grey'));
-                    //     console.log(players[playerIndex].getSkinEntries());
-                    // });
-                    
-                    if (playerIndex == 0) {
-                        // players[playerIndex].skinChange(players[playerIndex].findSkin('Grey'));
-                    }
-                }
-
-                if (true) { //Update team colors
-                    // if (playerIndex == 0) {
-                    //     if (playerCount > 2) { //force red on teams
-                    //         currentColors.updateColor(0, getColorBasedOnSlot(1, 'HMN'));
-                    //     } else {
-                    //         currentColors.updateColor(0, getColorBasedOnSlot(slot, state));
-                    //     }
-                    // } else if (playerIndex == 1 && playerCount < 3) {
-                    //     currentColors.updateColor(1, getColorBasedOnSlot(slot, state));
-                    // } else if (playerIndex == 2 && playerCount > 2) {
-                    //     currentColors.updateColor(1, getColorBasedOnSlot(2, 'HMN'));
-                    // }
-                }
-
-                if (true) { //Update Score
-                    if (playerIndex == 0) {
-                        scores[0].setScore(gameCount); 
-                    } else if (playerIndex == 1 && playerCount < 3) {
-                        scores[1].setScore(gameCount); 
-                    } else if (playerIndex == 2 && playerCount == 4) {
-                        scores[1].setScore(gameCount); 
-                    }
-                }
-
+                await this.#setCharAndSkinData(playerIndex, char);
+                await this.#setColorData(playerIndex, playerCount, slot, state);
+                await this.#setScoreData(playerIndex, playerCount, gameCount);
                 playerIndex++; 
             }
 
-            if (bestOf.getBo() == 3 && (scores[0].getScore() >=2 || scores[1].getScore() >=2)) {
-                settings.toggleScreenRocker();
-            }
-
-            if (bestOf.getBo() == 5 && (scores[0].getScore() >=3 || scores[1].getScore() >=3)) {
-                settings.toggleScreenRocker();
+            if(!this.canToggleOn()) {
+                settings.toggleScreenRocker()
             }
         }
 
-        //Best of
-        if (this.data.TourneySet.TourneyModeBestOf == 3 || this.data.TourneySet.TourneyModeBestOf == 5) {
-            bestOf.setBo(this.data.TourneySet.TourneyModeBestOf );
-        }
+        this.#setBoData(this.#data.TourneySet.TourneyModeBestOf);
+        this.#writeData();
     }
+    
 }
 
 
@@ -140,11 +253,12 @@ class ScreenRocker {
 setInterval(init, 1000);
 
 function init() {
-    if (screenRocker.enabled) {
-        screenRocker.getData();
-        if (screenRocker.data && screenRocker.playerPresets) {
-            screenRocker.setData();
-        }
+    if (screenRocker.enabled()) {
+        screenRocker.getData().then(() => {
+            if (screenRocker.hasData) {
+                screenRocker.setData();
+            }
+        });
     }
 }
 
@@ -155,14 +269,5 @@ function capitalizeWords(str) {
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ');
   }
-
-function getColorBasedOnSlot(slot, state) {
-    // if (state == 'CPU') {
-    //     return currentColors;
-    // }
-    // if (slot == 1) {
-
-    // }
-}
 
 export const screenRocker = new ScreenRocker;
